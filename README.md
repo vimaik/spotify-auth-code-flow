eb846c0720f054a08979a84ec9f3ea5380d207a3
vimaik <victormg2k@gmail.com>
2023-12-12 11:33:42 +0100

    14. Spotify: Authorization Code Flow (Resuelto).

---
+++ README.md
@@ -0,0 +1,9 @@
+# Configuración inicial y arranque
+## Instalar dependencias
+$ npm install
+
+## Ejecutar la aplicación en modo desarrollo (DEV >> puerto:3002)
+$ npm run dev
+
+## Ejecutar la aplicación en modo producción (PROD >> Puerto 3000)
+$ npm run
---
+++ pages/api/login.js
@@ -0,0 +1,21 @@
+import querystring from "node:querystring";
+
+const SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize";
+
+export default function handler(req, res) {
+  const scopes = [
+    "user-read-private",
+    "user-read-email",
+    "user-read-playback-state"
+  ];
+
+  const query = querystring.stringify({
+    response_type: "code",
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    scopes: scopes.join(" "),
+    redirect_uri: process.env.REDIRECT_URI,
+  });
+
+  res.writeHead(302, { Location: `${SPOTIFY_AUTH_URL}?${query}` });
+  res.end();
+}
